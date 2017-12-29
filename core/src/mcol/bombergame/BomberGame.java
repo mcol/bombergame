1f9c2741aaf4fcf3bcdaa8595582bfee7e6d4875
Marco Colombo <m.colombo@ed.ac.uk>
2017-12-29 10:37:49 +0000

    Convert tabs to spaces.

--- core/src/mcol/bombergame/BomberGame.java
+++ core/src/mcol/bombergame/BomberGame.java
@@ -14,25 +14,25 @@ public class BomberGame extends ApplicationAdapter {
     public static final String TITLE = "Bomber";
 
     private GameStateManager gsm;
-	private SpriteBatch batch;
+    private SpriteBatch batch;
 
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
-		gsm = new GameStateManager();
-		gsm.push(new MenuState(gsm));
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        gsm = new GameStateManager();
+        gsm.push(new MenuState(gsm));
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+    }
 
-	@Override
-	public void render() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		gsm.update(Gdx.graphics.getDeltaTime());
-		gsm.render(batch);
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gsm.update(Gdx.graphics.getDeltaTime());
+        gsm.render(batch);
+    }
 
-	@Override
-	public void dispose() {
-		batch.dispose();
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }

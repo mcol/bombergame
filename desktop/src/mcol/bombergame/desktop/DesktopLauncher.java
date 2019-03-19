79ef77450b6b075bd5a62e29428cbe46053778c3
Marco Colombo <mar.colombo13@gmail.com>
2019-03-19 08:52:38 +0000

    Remove tabulations.

--- desktop/src/mcol/bombergame/desktop/DesktopLauncher.java
+++ desktop/src/mcol/bombergame/desktop/DesktopLauncher.java
@@ -6,12 +6,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import mcol.bombergame.BomberGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1280;
-		config.height = 720;
-		config.title = BomberGame.TITLE;
-		config.addIcon("icon.png", FileType.Internal);
-		new LwjglApplication(new BomberGame(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1280;
+        config.height = 720;
+        config.title = BomberGame.TITLE;
+        config.addIcon("icon.png", FileType.Internal);
+        new LwjglApplication(new BomberGame(), config);
+    }
 }
